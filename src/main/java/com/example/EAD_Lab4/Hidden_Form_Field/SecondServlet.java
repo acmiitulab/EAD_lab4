5e3c3b4c8f4f606581fd73dc863a3d2f55732dac
mukhametkaly <60399541+mukhametkaly@users.noreply.github.com>
2021-02-27 23:08:57 +0600

    lab4

---
+++ src/main/java/com/example/EAD_Lab4/Hidden_Form_Field/SecondServlet.java
@@ -0,0 +1,24 @@
+package com.example.EAD_Lab4.Hidden_Form_Field;
+
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+
+public class SecondServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            response.setContentType("text/html");
+            PrintWriter out = response.getWriter();
+
+            //Getting the value from the hidden field
+            String n=request.getParameter("uname");
+            out.print("Hello "+n);
+
+            out.close();
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+    }
+}
